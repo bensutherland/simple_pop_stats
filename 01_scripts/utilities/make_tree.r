bd95b342bd96ebce25e2cd176757e8d4e862a5df
bensutherland <bensutherland7@gmail.com>
2019-06-07 16:02:34 -0700

    Create make_tree.r

---
+++ 01_scripts/utilities/make_tree.r
@@ -0,0 +1,77 @@
+# Construct dendrograms based on genetic differentiation
+# Note, if following readme, use boot_obj as "obj_filt"
+# Note, if following readme, use matrix as "pairwise_wc_fst"
+
+make_tree <- function(matrix = NULL
+                              , tree_method = "NJ"
+                              , bootstrap = FALSE, nboots = 10000, boot_obj = NULL
+                              , dist_metric = "edwards.dist"
+                              , separated = FALSE){
+  
+  if(tree_method=="NJ" && bootstrap==FALSE){
+    
+    # Construct unrooted tree with NJ
+    nj.tree <- NJ(matrix)
+    
+    # Save out, create filename
+    if(separated==TRUE){
+      
+      fn.plot <- paste0("03_results/gen_diff_tree_nj_unrooted_", sep_by, "_by_", name_by, ".pdf")
+      fn.tree <- paste0("03_results/gen_diff_tree_nj_unrooted_", sep_by, "_by_", name_by, ".tre")
+      
+    }else{
+      
+      fn.plot <- paste0("03_results/gen_diff_tree_nj_unrooted.pdf")
+      fn.tree <- paste0("03_results/gen_diff_tree_nj_unrooted.tre")
+      
+    }
+    
+    # Save plot
+    pdf(file = fn.plot)
+    plot(nj.tree, "unrooted"
+         #, main=""
+         #, cex = 1.3
+    )
+    dev.off()
+    
+    # Save tree
+    write.tree(nj.tree, file=fn.tree)
+    
+  }
+  
+  if(tree_method=="NJ" && bootstrap==TRUE){
+    
+    if(separated==TRUE){
+      
+      fn.plot <- paste0("03_results/gen_diff_tree_nj_unrooted_", sep_by, "_by_", name_by, "_", dist_metric, "_", nboots, "_boots.pdf")
+      fn.tree <- paste0("03_results/gen_diff_tree_nj_unrooted_", sep_by, "_by_", name_by, "_", dist_metric, "_", nboots, "_boots.tre")
+      
+    }else{
+      
+      fn.plot <- paste0("03_results/gen_diff_tree_nj_unrooted_", dist_metric, "_", nboots, "_boots.pdf")
+      fn.tree <- paste0("03_results/gen_diff_tree_nj_unrooted_", dist_metric, "_", nboots, "_boots.tre")
+      
+    }
+    
+    # Save plot
+    pdf(file = fn.plot)
+    bootstrapped_tree <- aboot(x = boot_obj, dist = dist_metric, sample = nboots, strata = pop(boot_obj)
+                               , tree = tree_method)
+    dev.off()
+    
+    # Save tree
+    write.tree(phy = bootstrapped_tree, file = fn.tree)
+    
+  }
+  
+  assign(x = "bootstrapped_tree", value = bootstrapped_tree, envir = .GlobalEnv)
+  
+}
+
+
+
+
+
+
+
+
