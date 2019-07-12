dc8cc5f745c2509a6593305f49281a34a68f35f3
bensutherland <bensutherland7@gmail.com>
2019-07-12 15:50:25 -0700

    Add method to get vals for each loading

--- 01_scripts/utilities/dapc_from_genind.r
+++ 01_scripts/utilities/dapc_from_genind.r
@@ -54,6 +54,24 @@ dapc_from_genind <- function(data = obj_pop_filt, plot_allele_loadings = TRUE){
   par(mfrow=c(1,1), mar=c(3,4,3,3))
   loadingplot(dapc$var.contr, thres=1e-3, las = 1, xlab = "Loci", ylab = "Locus contribution", las = 1, main = "")
   dev.off()
+  
+  
+  dapc_loadings_all <- loadingplot(dapc1$var.contr, threshold = 0)
+  dapc_loadings_vals <- dapc_loadings_all$var.values # obtain var.contr vals
+  names(dapc_loadings_vals) <- names(dapc1$pca.cent) # bring in loci names instead of just index
+  head(dapc_loadings_vals)
+  dapc_loadings_vals <- as.data.frame(dapc_loadings_vals) # make a df
+  head(dapc_loadings_vals)
+  colnames(dapc_loadings_vals) <- "var.contr" # rename column
+  head(dapc_loadings_vals)
+  dapc_loadings_vals$mname <- rownames(x = dapc_loadings_vals) # make rownames a vector within df
+  head(dapc_loadings_vals)
+  
+  # Write out, without rownames
+  write.table(x = dapc_loadings_vals, file = paste0("03_results/", "dapc_loadings.csv")
+              , sep = ",", quote = F, col.names = T, row.names = F)
+  
+
   }
   
 }
