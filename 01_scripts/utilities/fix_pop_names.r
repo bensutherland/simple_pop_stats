51c06345a6282418b85c70836bb46f629cd51729
bensutherland <bensutherland7@gmail.com>
2019-07-24 10:23:56 -0700

    Remove forward slash characters from usat popnames

--- 01_scripts/utilities/fix_pop_names.r
+++ 01_scripts/utilities/fix_pop_names.r
@@ -1,14 +1,20 @@
 # Rename microsat pops
 
 fix_pop_names <- function(df = obj){
-  print("Renaming populations")
+  print("Renaming populations...")
   
   # Inclusively replace everything after first space in vector
   all_pops <- as.character(pop(obj))
   all_pops <- sub(pattern = " .*", replacement = "", x = all_pops)
   
+  # Remove other characters
+  all_pops <- sub(pattern = "/.*", replacement = "", x = all_pops)
+  
   # Rename pop value in object
   pop(obj) <- all_pops
+  
+  print("Your populations are now named:   ")
+  print(unique(pop(obj)))
   
   assign(x = "obj", value = obj, envir = .GlobalEnv)
   
