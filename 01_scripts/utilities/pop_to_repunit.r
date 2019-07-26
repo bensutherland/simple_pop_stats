e1f1cece3fb29d803d95564f7c41c7f4309fba51
bensutherland <bensutherland7@gmail.com>
2019-07-26 12:52:12 -0700

    Automate build genind with repunit as pop

---
+++ 01_scripts/utilities/pop_to_repunit.r
@@ -0,0 +1,46 @@
+# Replace pop in genind from 'collection' to 'repunit'
+# Will use the stock codes file created in calculate_AMOVA
+
+pop_to_repunit <- function(data = obj){
+  
+  strat.FN <- "00_archive/unique_pops_w_repunit.txt"
+  
+  # Reporting
+  print("Your original data contains the following populations")
+  print(table(pop(data)))
+  
+  
+  if(file.exists(strat.FN)==TRUE){
+  
+    # Read in the repunit file
+    strat_obj <- read.table(file = strat.FN, header = T, sep = ",")
+    
+    # Identify the pop ID
+    pops <- as.data.frame(pop(data), stringsAsFactors = F)
+    colnames(pops) <- "collection"
+    
+    # Merge the strata info with the per-indiv pop info to add repunit to each indiv
+    strat_and_pops <- merge(x = pops, y = strat_obj, by= "collection", sort = F, all.x = T)
+    
+    # Rename
+    pop(data) <- strat_and_pops$repunit
+    
+    print("Your data will now be saved as 'obj_repunit'")
+    assign(x = "obj_repunit", value = data, envir = .GlobalEnv)
+    
+    # Reporting
+    print("Your new data contains the following populations")
+    print(table(pop(obj_repunit)))
+    
+    
+  } else if(file.exists(strat.FN)==FALSE){
+    
+    print("There is no strata file, build this using the calculate_AMOVA(build = TRUE) function")
+    
+  }
+}
+
+
+
+
+
