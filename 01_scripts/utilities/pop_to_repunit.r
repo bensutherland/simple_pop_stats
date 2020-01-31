99685973fe1342cf944c3c0a65eecb2d68747e4b
bensutherland <bensutherland7@gmail.com>
2020-01-31 13:42:32 -0800

    Update pop_to_repunit.r

--- 01_scripts/utilities/pop_to_repunit.r
+++ 01_scripts/utilities/pop_to_repunit.r
@@ -7,6 +7,8 @@ pop_to_repunit <- function(data = obj){
   print("Your original data contains the following populations")
   print(table(pop(data)))
   
+  # Hack solution, needs to be updated with test to see if file exists, if doesn't will crash
+  strat.FN <- "00_archive/unique_pops_w_repunit.csv"
   
   if(file.exists(strat.FN)==TRUE){
   
