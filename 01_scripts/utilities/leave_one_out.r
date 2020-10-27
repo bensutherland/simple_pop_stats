5f862496d31cb96bed27d86411bf193ee81018f7
erondeau <erondeau@uvic.ca>
2020-10-27 16:41:39 -0700

    Continue development of LOO

--- 01_scripts/utilities/leave_one_out.r
+++ 01_scripts/utilities/leave_one_out.r
@@ -1,31 +1,34 @@
 ## Running full-baseline leave-one-out
 
-leave_one_out <- function(){
+leave_one_out <- function(repunits_file=FALSE){
   
   #### 01. Load in baseline ####
-  
-  
-  
   # Select a baseline to analyse
   rubias_base.FN <- choose.files(caption = "Select a rubias formatted base file")
 
-  ## Load baseline data - assuming pre-filterd here.
-  rubias_base <- read_tsv(rubias_base.FN,guess_max=100000)
+  ## Load baseline data - assuming pre-filtered here.
+  rubias_base <- read_tsv(rubias_base.FN,guess_max=150000)
   
+  if(repunits_file==TRUE){
+    # Load repunits file
+    repunit_desc.FN <- choose.files(getwd(),caption = "Path to repunits for analysis")
+   
+    # Load repunits file specified in config
+    print("Loading Reporting Units Detail")
+    repunits <- read_tsv(repunit_desc.FN)
+   
+    # Keep the Display_Order for downstream sorting
+    Display_Order <- repunits[c("repunit","Display_Order")]
+   
+  }
   
-  # Reduce repunits
-  repunit_desc.FN <- choose.files(getwd(),caption = "Path to repunits for analysis")
-  
-  # Load repunits file specified in config
-  print("Loading Reporting Units Detail")
-  repunits <- read_tsv(repunit_desc.FN)
-  
-  Display_Order <- repunits[c("repunit","Display_Order")]
-  
-  
-  
+  # Find a list of the unique collections and repunits
   colls <- unique(rubias_base[, c("collection", "repunit")])
+  
+  # Create an identical file for downstream labelling
   coll_infer <- colls
+  
+  # Change the names to make it obvious as to what they mean
   colnames(coll_infer) <- c("inferred_collection","inferred_repunit")
   
   # Format cols to character or integer as required for rubias; based on the indiv col
@@ -38,88 +41,61 @@ leave_one_out <- function(){
                                                 
   )
   
-  best_assignment <- all_collection_results %>% 
+  # Group by inferred repunit and summarize - this is directly from Rubias read-me.
+  # This creates a summary of likelihoods to repunits
+  best_assignment_repu <- all_collection_results %>%
+    group_by(indiv, collection, repunit, inferred_repunit) %>%
+    summarise(repu_scaled_like = sum(scaled_likelihood))
+
+  # Our goal here is to take the "best" assignment to repunit
+  # There was some question as to whether this is the correct way to represent the data.
+  # We decided this likely approximates the Oncor method as closely as we can.
+  best_assignment_repu <- best_assignment_repu %>% 
     group_by(indiv) %>% 
-    filter(scaled_likelihood == max(scaled_likelihood)) %>% 
+    filter(repu_scaled_like == max(repu_scaled_like)) %>% 
     distinct
   
+  # Do the same for "collection" - take the "best" assignment to collection. 
+  best_assignment_coll <- all_collection_results %>%
+    group_by(indiv) %>%
+    filter(scaled_likelihood == max(scaled_likelihood)) %>%
+    distinct
   
-  #### Collect all results to collection 
-  best_assignment_sum <- best_assignment %>%
-    group_by(collection,inferred_collection) %>%
-    tally() %>%
-    mutate(freq = n / sum(n)* 100)
-  
-  best_assignment_sum <- merge(best_assignment_sum,colls)
-  best_assignment_sum <- merge(best_assignment_sum,coll_infer)
-
-  best_assignment_sum_rep <- best_assignment %>%
+  # Count the number of assignments, summarized by "collection" individual was pulled from, and "inferred repunits" to which it assigned. 
+  best_assignment_repu_sum <- best_assignment_repu %>%
     group_by(collection,inferred_repunit) %>%
     tally() %>%
     mutate(freq = n / sum(n)* 100)
   
-  best_assignment_sum_rep <- merge(best_assignment_sum_rep,colls)
-  
-  best_assignment_sum_rep_rep <- best_assignment %>%
-    group_by(repunit,inferred_repunit) %>%
+  # Count the number of assignments, summarized by "collection" individual was pulled from, and "inferred collection" to which it assigned. 
+  best_assignment_coll_sum <- best_assignment_coll %>%
+    group_by(collection,inferred_collection) %>%
     tally() %>%
-    mutate(freq = n / sum(n) * 100)
-  
-  best_assignment_matrix <- dcast(best_assignment_sum,collection+repunit~inferred_collection,value.var="freq")
-  best_assignment_matrix_rep <- dcast(best_assignment_sum_rep,collection+repunit~inferred_repunit,value.var="freq")
-  best_assignment_matrix_rep_rep <- dcast(best_assignment_sum_rep_rep,repunit~inferred_repunit,value.var="freq")
-  
-  # Write out full-summary results
-  write_tsv(x = best_assignment_matrix, path = paste0(result.path, "all_collection_LOO_results_coll_coll_matrix_", format(Sys.time(), "%Y-%m-%d"),".txt"),na="")
-  write_tsv(x = best_assignment_matrix_rep, path = paste0(result.path, "all_collection_LOO_results_coll_rep_matrix_", format(Sys.time(), "%Y-%m-%d"),".txt"),na="")
-  write_tsv(x = best_assignment_matrix_rep_rep, path = paste0(result.path, "all_collection_LOO_results_rep_rep_matrix_", format(Sys.time(), "%Y-%m-%d"),".txt"),na="")
-  
-  
+    mutate(freq = n / sum(n)* 100)
 
-  #### Collect all results to collection 
-  coll_to_coll_filt_all <- all_collection_results %>%
-    group_by(repunit,collection, inferred_repunit, inferred_collection) %>%
-    summarise_at(.vars=c("scaled_likelihood")
-                 , .funs=c(mean_scaled_likelihood_coll="mean")) %>%
-    group_by(collection)
-  
-  
-  
-  
-  
-  # Collect all results to repunit
-  coll_to_rep <- coll_to_coll_filt_all %>%
-    group_by(collection, inferred_repunit) %>%
-    summarise_at(.vars=c("mean_scaled_likelihood_coll")
-                 , .funs=c(mean_scaled_likelihood_repunit="sum")) 
-  
-  # Re-sort so that results are easy to view
-  coll_to_coll_filt_all_sort <- coll_to_coll_filt_all[with(coll_to_coll_filt_all, order(collection, -mean_scaled_likelihood_coll)),]
-  
-  
+  # Use the tables created earlier to add back in the repunit ID to the collection and inferred_collection in the summary table
+  best_assignment_coll_sum <- merge(best_assignment_coll_sum,colls)
+  best_assignment_coll_sum <- merge(best_assignment_coll_sum,coll_infer)
 
+  # Use the tables created earlier to add back in the repunit ID to the collection - the inferred_repunit was already included (this is a to-repunit summary!)
+  best_assignment_repu_sum <- merge(best_assignment_repu_sum,colls)
+  # 
+  ########
+  # Leftovers from trying to create a matrix - keep it in in case needed in the future. 
+  # best_assignment_matrix <- dcast(best_assignment_sum,collection+repunit~inferred_collection,value.var="freq")
+  # best_assignment_matrix_rep <- dcast(best_assignment_sum_rep,collection+repunit~inferred_repunit,value.var="freq")
+  # best_assignment_matrix_rep_rep <- dcast(best_assignment_sum_rep_rep,repunit~inferred_repunit,value.var="freq")
+  # 
+  # # Write out full-summary results
+  # write_tsv(x = best_assignment_matrix, path = paste0(result.path, "all_collection_LOO_results_coll_coll_matrix_", format(Sys.time(), "%Y-%m-%d"),".txt"),na="")
+  # write_tsv(x = best_assignment_matrix_rep, path = paste0(result.path, "all_collection_LOO_results_coll_rep_matrix_", format(Sys.time(), "%Y-%m-%d"),".txt"),na="")
+  # write_tsv(x = best_assignment_matrix_rep_rep, path = paste0(result.path, "all_collection_LOO_results_rep_rep_matrix_", format(Sys.time(), "%Y-%m-%d"),".txt"),na="")
+  # 
+  ########
   
-  # Add in summary to repunit
-  coll_to_coll_filt_all_sort <- merge(coll_to_coll_filt_all_sort,coll_to_rep)
+  write_tsv(x = best_assignment_coll_sum, path = paste0(result.path, "all_collection_LOO_results_all-coll_", format(Sys.time(), "%Y-%m-%d"),".txt"),na="")
   
-  # Create a 1-line summary per collection
-  coll_to_coll_match <- coll_to_coll_filt_all_sort[coll_to_coll_filt_all_sort$collection == coll_to_coll_filt_all_sort$inferred_collection,]
-  
-  # Write out full-summary results
-  write_tsv(x = coll_to_coll_filt_all_sort, path = paste0(result.path, "all_collection_LOO_results_", format(Sys.time(), "%Y-%m-%d"),".txt"))
-  
-  # Write out the 1-line summary per collection
-  write_tsv(x = coll_to_coll_match, path = paste0(result.path, "collection_100_LOO_stats_", format(Sys.time(), "%Y-%m-%d"),".txt"))
-  
-  coll_to_coll_filt_all_matrix <- dcast(coll_to_coll_filt_all_sort,collection+repunit~inferred_collection,value.var="mean_scaled_likelihood_coll")
-  coll_to_coll_filt_all_matrix <- merge(coll_to_coll_filt_all_matrix, Display_Order)
-  
-  collections <- coll_to_coll_filt_all_matrix[c("collection","repunit","Display_Order")]
-  
-  
-  collections_t <- t(collections)
-  
-  write_tsv(x = coll_to_coll_filt_all_matrix, path = paste0(result.path, "collection_100_LOO_stats_all_pops_matrix_",format(Sys.time(), "%Y-%m-%d"),".txt"))
+  write_tsv(x = best_assignment_repu_sum, path = paste0(result.path, "all_collection_LOO_results_all-reps_", format(Sys.time(), "%Y-%m-%d"),".txt"),na="")
   
   
 }
