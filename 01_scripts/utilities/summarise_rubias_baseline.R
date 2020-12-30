6649e33c7e2d49c22d5444ac433380f27873e582
bensutherland <bensutherland7@gmail.com>
2020-12-30 10:59:24 -0800

    Improve script for microsat utility and minor formatting

--- 01_scripts/utilities/summarise_rubias_baseline.R
+++ 01_scripts/utilities/summarise_rubias_baseline.R
@@ -2,98 +2,106 @@
 # Assumes any necessary filtering has been done - see "clean_format_rubias" function for more on this
 # Will filter by repunit if the repunit_desc file does not contain all the repunits
 
+summarise_rubias_baseline <- function(baseline = NULL, repunit_desc = NULL
+                                      , out_prefix = "rubias_base_summary", by_year=FALSE
+                                      , type = "SNP"){
 
-#repunit_desc <- read.delim(file="H:/Stock_Codes/chinook/repunits_full.txt", stringsAsFactors = FALSE)
-
-
-summarise_rubias_baseline <- function(out_prefix = "rubias_base_summary",
-                               by_year=FALSE, type = "SNP"){
-  library(readr)
+    # Set baseline FN if defined
+    base.fn <- baseline  
+  
+    # If the baseline is not defined, choose it
+    if(is.null(base.fn)){
+      
+      # Select the file
+      base.fn <- choose.files(caption = "Select a rubias formatted base file")
+      
+    }
     
-
-    
-    # If not, select the file
-    base.fn <- choose.files(caption = "Select a rubias formatted base file")
-    
-    # Load the rubias base file
+    # Load the baseline
     baseline <- read_tsv(file=base.fn,guess_max = 100000)
   
-  
-  
-  
-
-    if (type=="SNP"){
-        # Reduce repunits
-        repunit_desc.FN <- choose.files(getwd(),caption = "Path to repunits for analysis")
     
-        # Load repunits file specified in config
-        print("Loading Reporting Units Detail")
-        repunits <- read_tsv(repunit_desc.FN)
+    # Set repunit FN if defined
+    repunit_desc.FN <- repunit_desc
+    
+    # If the repunit is not defined, choose it
+    if(is.null(repunit_desc.FN)){
+      
+      # Reduce repunits
+      repunit_desc.FN <- choose.files(getwd(),caption = "Path to repunits for analysis")
+      
     }
-  
-  
-  # Load necessary libraries
-    library(dplyr)
-    library(reshape2)
-   
+    
+    # Load repunits file specified
+    print("Loading Reporting Units Detail")
+    repunits <- read_tsv(repunit_desc.FN)
+    
+    ### Can this be removed? ###
     # Create a backup of the baseline
-    baseline_bck <- baseline
+    # baseline_bck <- baseline
+    ### /END/ Can this be removed? /END/ ###
     
-    
-    if (type=="SNP"){
-        # Split the individual ID to get the collection year
-        baseline$year <- baseline$indiv %>%
+    # Get collection year
+    if(type=="SNP"){
+        
+      # Split the individual ID to get the collection year
+      baseline$year <- baseline$indiv %>%
                                   strsplit("_") %>%
                                   sapply( "[", 2 )
     
-    } else if (type=="microsat"){
+    }else if(type=="microsat"){
         
         # Replace the 999 (unknown year) with a 4 digit version
         baseline$indiv <- gsub("_999_", "_9999_", baseline$indiv)
         
         # Find the 4 digit values, and keep the first as the "year"
-        baseline$year <- as.data.frame(str_extract_all(baseline$indiv,"\\d{4}", simplify = T)
-                                      ,stringsAsFactors = FALSE)[[1]]
+        year.vec <- str_extract_all(string = baseline$indiv, pattern = "\\d{4}", simplify = T)
+        # Add to baseline
+        baseline$year <- as.data.frame(x = year.vec, stringsAsFactors = FALSE)[[1]]
         
         # If any years were missing (999 code or NA), replace with "unknown"
         baseline$year[baseline$year==""] <- "unknown"
         baseline$year[baseline$year=="9999"] <- "unknown"
     }
     
-    # Reduce the dataframe to the necessary columsns
-    baseline_reduced <- subset(baseline, select = c("collection","repunit","year"))
+    # Reduce the dataframe to the necessary columns
+    cols_to_retain <- c("collection","repunit","year")
+    baseline_reduced <- subset(baseline, select = cols_to_retain)
     
     # Dcast the table to summarize by year
-    base_summary <- reshape2::dcast(baseline_reduced,collection+repunit~year,value.var="year",fun.aggregate = length)
+    base_summary <- reshape2::dcast(baseline_reduced, collection + repunit ~ year
+                                    , value.var="year"
+                                    , fun.aggregate = length
+                                    )
 
-    # Count the number of fish in the "year" columns
-    base_summary$total_N <- rowSums(base_summary[,3:length(colnames(base_summary))])
+    # Count the number of fish in the "year" columns (starts counting at first year column)
+    base_summary$total_N <- rowSums(base_summary[, length(cols_to_retain):ncol(base_summary)])
 
     # Add a column for the year string
     base_summary$Years <- NA
 
+    # Prepare which columns need to be removed below
+    cols_to_drop <- c("collection", "repunit", "total_N", "Years")
+    
     # Create the year string per collection.
-    for (i in 1:length(row.names(base_summary))){
+    for (i in 1:nrow(base_summary)){
       
           # Extract the line per collection
           work <- base_summary[i,]
           
           # Remove the first 2 columns repunit and collection
-          work <- work[,3:length(work)]
-          
-          # Remove the last 2 columns Years and total_N
-          work <- work[,-(length(work))]
-          work <- work[,-(length(work))]
+          work <- work[,-(which(colnames(work) %in% cols_to_drop))]
           
           # Remove any columns that are == 0
           work <- work %>% select_if(~any(. > 0))
           
+          # Summarize by year if requested
           if(by_year==TRUE){
                 for (j in 1:length(work)){
                       work[j] <- paste0(colnames(work)[j],"(",work[j],")")
                 }
                 
-                base_summary$Years[i] <- paste(work,collapse=", ")
+                base_summary$Years[i] <- paste(work, collapse=", ")
           } else {
             
             # Create a string of remaining column headers
@@ -103,55 +111,74 @@ summarise_rubias_baseline <- function(out_prefix = "rubias_base_summary",
             
     }
 
-    if (type=="SNP"){
+    # Collecting all results (SNP)
+    if(type=="SNP"){
     
-    # Merge the results with the repunits file
-    base_summary <- merge(base_summary,repunits,by="repunit")
+      # Merge the results with the repunits file
+      base_summary <- merge(base_summary, repunits, by="repunit")
 
-    # Order based on the Display_Order column
-    base_summary <- base_summary[order(base_summary$Display_Order,base_summary$collection),]
+      # Order rows based on the Display_Order column
+      base_summary <- base_summary[order(base_summary$Display_Order, base_summary$collection), ]
 
-    # Keep the columns of interest
-    if("region" %in% colnames(base_summary)){
-      base_summary <- subset(base_summary, select = c("CU_NAME","CU","region","collection","Years","total_N"))
+      # Keep the columns of interest if region is included
+      if("region" %in% colnames(base_summary)){
+        
+        base_summary <- subset(base_summary, select = c("CU_NAME","CU","region","collection","Years","total_N"))
       
-      if(by_year==TRUE){
-        # Rename the columns of interest
-        colnames(base_summary) <- c("Repunit/Conservation Unit","CU Number","Region","Population","Years(N)","N")
+        if(by_year==TRUE){
+        
+          # Rename the columns of interest
+          colnames(base_summary) <- c("Repunit/Conservation Unit","CU Number","Region","Population","Years(N)","N")
+          
+        } else {
+          
+          # Rename the columns of interest
+          colnames(base_summary) <- c("Repunit/Conservation Unit","CU Number","Region","Population","Years","N")
+          
+        }
+      
+        base_sum_orig <- base_summary
+      
+        # Blank out repeated repunit and CU numbers
+        base_summary$`Repunit/Conservation Unit`[duplicated(base_summary$`Repunit/Conservation Unit`)] <- ""
+        base_summary$`CU Number`[duplicated(base_summary$`CU Number`)] <- ""
+        base_summary$`Region`[duplicated(base_sum_orig$`CU Number`)] <- ""
+      
+      # Keep the columns of interest if region is not included in base_summary
       } else {
-        # Rename the columns of interest
-        colnames(base_summary) <- c("Repunit/Conservation Unit","CU Number","Region","Population","Years","N")
+      
+        base_summary <- subset(base_summary, select = c("CU_NAME","CU","collection","Years","total_N"))
+        
+        # If retained by-year counts
+        if(by_year==TRUE){
+          
+          # Rename the columns of interest
+          colnames(base_summary) <- c("Repunit/Conservation Unit","CU Number","Population","Years(N)","N")
+        } else {
+          
+          # Rename the columns of interest
+          colnames(base_summary) <- c("Repunit/Conservation Unit","CU Number","Population","Years","N")
+        }
+      
+        # Blank out repeated repunit and CU numbers
+        base_summary$`Repunit/Conservation Unit`[duplicated(base_summary$`Repunit/Conservation Unit`)] <- ""
+        base_summary$`CU Number`[duplicated(base_summary$`CU Number`)] <- ""
+      
       }
       
-      base_sum_orig <- base_summary
-      # Blank out repeated repunit and CU numbers
-      base_summary$`Repunit/Conservation Unit`[duplicated(base_summary$`Repunit/Conservation Unit`)] <- ""
-      base_summary$`CU Number`[duplicated(base_summary$`CU Number`)] <- ""
-      base_summary$`Region`[duplicated(base_sum_orig$`CU Number`)] <- ""
-      
-      
-    } else {
-      
-      base_summary <- subset(base_summary, select = c("CU_NAME","CU","collection","Years","total_N"))
-      if(by_year==TRUE){
-        # Rename the columns of interest
-        colnames(base_summary) <- c("Repunit/Conservation Unit","CU Number","Population","Years(N)","N")
-      } else {
-        # Rename the columns of interest
-        colnames(base_summary) <- c("Repunit/Conservation Unit","CU Number","Population","Years","N")
-      }
-      
-      # Blank out repeated repunit and CU numbers
-      base_summary$`Repunit/Conservation Unit`[duplicated(base_summary$`Repunit/Conservation Unit`)] <- ""
-      base_summary$`CU Number`[duplicated(base_summary$`CU Number`)] <- ""
-      
-    }
-    } else {
+    # # Collecting all results (microsat)
+    } else if(type=="microsat"){
 
+      # Merge the results with the repunits file
+      base_summary <- merge(base_summary, repunits, by="repunit")
+      
       # Order based on the Display_Order column
-      base_summary <- base_summary[order(base_summary$repunit,base_summary$collection),]
+      base_summary <- base_summary[order(base_summary$Display_Order, base_summary$collection), ]
       
+      # Keep only the necessar cols
       base_summary <- subset(base_summary, select = c("repunit","collection","Years","total_N"))
+      
+      # Rename cols depending on if count by year is on
       if(by_year==TRUE){
         # Rename the columns of interest
         colnames(base_summary) <- c("Region","Population","Years(N)","N")
@@ -167,10 +194,15 @@ summarise_rubias_baseline <- function(out_prefix = "rubias_base_summary",
     }
 
     
-
-
+    # Set output filename
+    output.FN <- paste0(result.path, out_prefix,".baseline_summary.txt")
+    
+    # Reporting
+    print(paste0("Exporting results to: ", output.FN))
+    
     # Output the dataframe
-    write.table(base_summary,file=paste0("03_results/",out_prefix,".baseline_summary.txt")
-                ,quote=FALSE,sep="\t",row.names=FALSE)
+    write.table(base_summary,file=output.FN
+                , quote=FALSE, sep="\t", row.names=FALSE
+                )
 
 }
