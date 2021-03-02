2ef70a1288772d274f77c64caf12383eda7e9abb
Ben Sutherland <bensutherland7@gmail.com>
2021-03-02 07:29:38 -0800

    Improve reporting, reduce verbosity

--- 01_scripts/utilities/format_sims_output.R
+++ 01_scripts/utilities/format_sims_output.R
@@ -2,14 +2,14 @@
 format_sims_output <- function(sims.FN = NULL, repunit = NULL){
 
   # Reporting
-  print("Merging sims output with repunit file")
+  print(paste0("Merging sims output with repunit file for ", species))
   
   # Set repunit filename
   repunit.FN <- paste0("H:/Stock_Codes/", species, "/repunits_full.txt")
   
   # Read in repunits file
   repunit.df <- read.delim2(file = repunit.FN, header = T, sep = "\t", stringsAsFactors = FALSE)
-  head(repunit.df)
+  #head(repunit.df)
   
   # Manually read in sims file (function depends windows or linux)
   print("Select your sims (stats) output from file...")
@@ -25,15 +25,16 @@ format_sims_output <- function(sims.FN = NULL, repunit = NULL){
   }
   
   sims.df <- read.delim2(file = sims.FN, header = T, sep = "\t", stringsAsFactors = FALSE)
-  head(sims.df)
+  #head(sims.df)
   
   # Merge and make dataframe
   all_data <- merge(x = repunit.df, y = sims.df, by = "repunit", all.y = T)
   all_data.df  <- as.data.frame(x = all_data, stringsAsFactors = FALSE)
-  head(all_data.df)
-  str(all_data.df)
+  #head(all_data.df)
+  #str(all_data.df)
   
   # Sort by display order, then by ascending repunit assignment
+  print("Sorting by display order, then by repunit assignment percentage")
   all_data.df <- all_data.df[order(all_data.df$Display_Order, all_data.df$repunit_post_mean_pi),]
   head(all_data.df)
   
@@ -42,6 +43,7 @@ format_sims_output <- function(sims.FN = NULL, repunit = NULL){
   output.FN <- gsub(pattern = "\\.txt", replacement = "_sorted.txt", x = output.FN)
   
   # Write output
+  print(paste0("Writing result to ", output.FN))
   write.table(x = all_data.df, file = paste0(result.path, output.FN)
               , quote = FALSE, sep = "\t", col.names = T, row.names = F)
   
