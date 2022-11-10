5af9d26be4eed24d8893abfb6465c21f19198924
bensutherland <bensutherland7@gmail.com>
2022-11-10 14:02:47 -0800

    Increase reliability of joins via merge and include output as a global variable in addition to writing out

--- 01_scripts/utilities/per_locus_stats.r
+++ 01_scripts/utilities/per_locus_stats.r
@@ -5,28 +5,30 @@ per_locus_stats <- function( data = obj ){
   # Calculate per-locus Fst
   print("Calculating per-locus FST")
   obj_fst <- Fst(as.loci(obj)) # extract the loci from genepop obj and calculate FST per locus
-  # produces a matrix (number loci x 3 (Fit, Fst, Fis))
+  obj_fst <- as.data.frame(obj_fst)
+  obj_fst$mname <- rownames(obj_fst)
+  # produces a df (number loci x 3 (Fit, Fst, Fis))
   
   # Additional calculations
   print("Calculating additional per-locus stats")
   obj_sum <- summary(obj) # this is a large genind summary, complex obj with names ("n", "n.by.pop", "loc.n.all", "pop.n.all", "NA.perc", "Hobs", "Hexp")
   
-  # Bring in per locus Hobs to the per locus summary
-  obj_fst_h <- cbind(obj_fst, obj_sum$Hobs)
-  head(obj_fst_h)
-  colnames(obj_fst_h)[ncol(obj_fst_h)] <- "Hobs"
+  # Obtain hobs
+  hobs.df <- as.data.frame(obj_sum$Hobs)
+  hobs.df$mname <- rownames(hobs.df)
+  colnames(hobs.df) <- c("Hobs", "mname")
   
-  # Bring in per locus Hexp to the per locus summary
-  obj_fst_h <- cbind(obj_fst_h, obj_sum$Hexp)
-  head(obj_fst_h)
-  colnames(obj_fst_h)[ncol(obj_fst_h)] <- "Hexp"
-
-  # obj_fst_h.bck <- obj_fst_h
-  # obj_fst_h <- obj_fst_h.bck
+  # Obtain hexp
+  hexp.df <- as.data.frame(obj_sum$Hexp)
+  hexp.df$mname <- rownames(hexp.df)
+  colnames(hexp.df) <- c("Hexp", "mname")
   
-  # Reduce sig figs
-  for(i in 1:ncol(obj_fst_h)){
-    print(i)
+  # Join all per-locus stats
+  obj_fst_h <- merge(x = obj_fst, y = hobs.df, by = "mname")
+  obj_fst_h <- merge(x = obj_fst_h, y = hexp.df, by = "mname")
+  
+  # Reduce the numeric variables to four sig figs
+  for(i in 2:ncol(obj_fst_h)){
     
     obj_fst_h[,i] <- round(x = obj_fst_h[,i], digits = 4)
     
@@ -36,7 +38,12 @@ per_locus_stats <- function( data = obj ){
   print("Writing out to 03_results/per_locus_stats*.txt")
   write.table(obj_fst_h
               , file = paste0("03_results/per_locus_stats_", format(Sys.time(), "%Y-%m-%d"),".txt")
-              , quote = F, sep="\t", row.names = T, col.names = T
+              , quote = F, sep="\t", row.names = F, col.names = T
               )
+  
+  # Assign to global variable
+  print("Saving output to global environment as per_loc_stats.df")
+  assign(x = "per_loc_stats.df", value = obj_fst_h, envir = .GlobalEnv)
+  
 }
 
