08086223bd3952525071b5a305ea8d01f80b34bb
erondeau <45638213+erondeau@users.noreply.github.com>
2022-12-09 16:12:01 -0800

    Create interactive_map_from_stockcodes_file.R

---
+++ 01_scripts/dev/interactive_map_from_stockcodes_file.R
@@ -0,0 +1,43 @@
+#Making a map
+
+install.packages("tmap")
+install.packages("sf")
+install.packages("terra")
+install.packages("dplyr")
+install.packages("spData")
+install.packages("spDataLarge")
+install.packages("ggplot2")
+install.packages("leaflet")
+install.packages("leaflet.esri")
+
+library(sf)
+library(terra)
+library(dplyr)
+library(spData)
+#library(spDataLarge)
+library(tmap)
+library(leaflet)
+library(leaflet.esri)
+library(ggplot2)
+library(tidyverse)
+
+
+tmap_mode("view")
+
+
+stock.codes <- read.delim(file="H:/Stock_Codes/Chum/cmStockCodesCU.txt")
+stock.codes <- stock.codes %>% drop_na(YLAT)
+
+map_points <- stock.codes %>%
+  st_as_sf(coords = c("XLONG","YLAT")) %>% 
+  st_cast("POINT")
+
+map <- tm_shape(map_points) + 
+  tm_dots(group="ProvState",
+          col="ProvState",
+  palette = "Dark2",
+  popup.vars=TRUE) +
+  tm_basemap(server = "Esri.WorldTopoMap")
+map
+
+
