6ea2b90d1d2b180a1713fe8c582e04b460507e8d
bensutherland <bensutherland7@gmail.com>
2023-01-05 14:58:12 -0800

    Enable loadingplot of first two Discrim. Fn. if n.da > 1

--- 01_scripts/utilities/dapc_from_genind.r
+++ 01_scripts/utilities/dapc_from_genind.r
@@ -63,33 +63,62 @@ dapc_from_genind <- function(data = obj_pop_filt, plot_allele_loadings = TRUE
   
   # Loading plot # Plot marker variance contribution to DAPC
   if(plot_allele_loadings==TRUE){
-  filename <- paste("03_results/", "DAPC_loadings.pdf", sep = "")
-  pdf(file = filename, width = 11, height = 4)
-  par(mfrow=c(1,1), mar=c(3,4,3,3))
-  loadingplot(dapc$var.contr, thres=1e-3, las = 1, xlab = "Loci", ylab = "Locus contribution", las = 1, main = "")
-  dev.off()
-  
-  # Create loading plot
-  dapc_loadings_all <- loadingplot(dapc$var.contr, threshold = 0)
-  
-  # obtain variance contribution values and their corresponding names
-  dapc_loadings_vals <- dapc_loadings_all$var.values # obtain var.contr vals
-  
-  names(dapc_loadings_vals) <- dapc_loadings_all$var.names # bring in loci names instead of just index
-  head(dapc_loadings_vals)
-  
-  dapc_loadings_vals <- as.data.frame(dapc_loadings_vals) # make a df
-  head(dapc_loadings_vals)
-  colnames(dapc_loadings_vals) <- "var.contr" # rename column
-  head(dapc_loadings_vals)
-  
-  # Keep rownames in df
-  dapc_loadings_vals$mname <- rownames(x = dapc_loadings_vals) # make rownames a vector within df
-  head(dapc_loadings_vals)
-  
-  # Write out, without rownames
-  write.table(x = dapc_loadings_vals, file = paste0("03_results/", "dapc_loadings.csv")
-              , sep = ",", quote = F, col.names = T, row.names = F)
+    
+    # Set filename
+    filename <- paste("03_results/", "DAPC_loadings.pdf", sep = "")
+    
+    # Plot
+    # If there is only one DA, plot loadings for the DA, else
+    if(dapc$n.da==1){
+      
+      pdf(file = filename, width = 11, height = 4)
+      par(mfrow=c(1,1), mar=c(5,4,4,2) + 0.1)
+      loadingplot(x = dapc$var.contr, thres = 1e-3
+                  #, las = 1
+                  , xlab = "Markers", ylab = "Locus contribution"
+                  , main = "DF1 Variance Contribution"
+      )
+      dev.off()
+      
+    # If there are more than one DA, plot the first two
+    }else if(dapc$n.da > 1){
+      
+      pdf(file = filename, width = 11, height = 8)
+      par(mfrow=c(2,1), mar=c(5,4,4,2) + 0.1)
+      loadingplot(x = dapc$var.contr, thres = 1e-3
+                  , axis = 1
+                  , xlab = "Markers", ylab = "Locus contribution", main = "DF1 Variance Contribution"
+      )
+      loadingplot(x = dapc$var.contr, thres = 1e-3
+                  , axis = 2
+                  , xlab = "Markers", ylab = "Locus contribution", main = "DF2 Variance Contribution"
+      )
+      dev.off()
+      
+    }
+
+    
+    # Export the result of loadingplot function
+    dapc_loadings_all <- loadingplot(dapc$var.contr, threshold = 0)
+    
+    # obtain variance contribution values and their corresponding names
+    dapc_loadings_vals <- dapc_loadings_all$var.values # obtain var.contr vals
+    
+    names(dapc_loadings_vals) <- dapc_loadings_all$var.names # bring in loci names instead of just index
+    head(dapc_loadings_vals)
+    
+    dapc_loadings_vals <- as.data.frame(dapc_loadings_vals) # make a df
+    head(dapc_loadings_vals)
+    colnames(dapc_loadings_vals) <- "var.contr" # rename column
+    head(dapc_loadings_vals)
+    
+    # Keep rownames in df
+    dapc_loadings_vals$mname <- rownames(x = dapc_loadings_vals) # make rownames a vector within df
+    head(dapc_loadings_vals)
+    
+    # Write out, without rownames
+    write.table(x = dapc_loadings_vals, file = paste0("03_results/", "dapc_loadings.csv")
+                , sep = ",", quote = F, col.names = T, row.names = F)
   
 
   }
