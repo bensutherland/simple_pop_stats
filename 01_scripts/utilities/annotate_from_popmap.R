a47ab2c389bce915d9044d02eec37d6c8fb89612
bensutherland <bensutherland7@gmail.com>
2024-03-21 09:39:58 -0700

    Enable conversion to alt.ID

--- 01_scripts/utilities/annotate_from_popmap.R
+++ 01_scripts/utilities/annotate_from_popmap.R
@@ -1,7 +1,8 @@
 # Load popmap and add sample attributes
 # Sutherland Bioinformatics, 2024-03-17
 
-annotate_from_popmap <- function(df = obj, popmap.FN = "00_archive/my_data_ind-to-pop_annot.txt"){
+annotate_from_popmap <- function(df = obj, popmap.FN = "00_archive/my_data_ind-to-pop_annot.txt"
+                                 , convert_to_alt_ID = F){
 
   ## Load annotated df
   indiv_annot.df <- read.table(file = popmap.FN, header = T, sep = "\t")
@@ -35,8 +36,30 @@ annotate_from_popmap <- function(df = obj, popmap.FN = "00_archive/my_data_ind-t
   # Update the pop attribute from the ordered sample metadata
   pop(df) <- indiv_annot_in_order.df[, "pop"]
   
+  # Convert to alternate ID if selected
+  if(convert_to_alt_ID == TRUE){
+    
+    # Reporting
+    print("Updating identifiers to alternate IDs")
+    
+    # Updating to alternate identifiers
+    indNames(df) <- indiv_annot_in_order.df[, "alt.ID"]
+    
+    # Reporting
+    print("Identifier format is now: ")
+    print(head(unique(indNames(df))))
+    
+  }else{
+    
+    print("Retaining original identifiers")
+    
+  }
+  
   # Reporting
   print(table((pop(df))))
+  
+  
+  
   
   print("Saving annotated obj as obj_annot")
   assign(x = "obj_annot", value = df, envir = .GlobalEnv)
